424f5c5b1216eb204cdaf7bb87cb65d44ae81605
ecohcm <parkhcm8@gmail.com>
2026-03-23 00:17:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ TELEGRAM_TOKEN = "8418423317:AAHodaij34Zu5MZciHWLBXbgAUzKBkUL4Rs"
 YOUR_CHAT_ID = "7737429021"
 
 CURRENT_PIN = "1234"
-CHANGE_PIN = "000000"
+CHANGE_PIN = "851215"
 
 API_KEY = "AIzaSyD9JqlO1r4WozGod_vd5R6DOQB_HRits18"
 
@@ -91,7 +91,7 @@ def show_main_site():
 <html lang="ko">
 <head>
     <meta charset="UTF-8">
-    <title>사진 예약 사이트</title>
+    <title>사ecohcm</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <style>
@@ -103,7 +103,7 @@ def show_main_site():
 </head>
 <body class="bg-gray-900 text-white">
 <div class="max-w-7xl mx-auto p-6">
-    <h1 class="text-5xl font-bold text-center my-10">📸 사진 예약 사이트</h1>
+    <h1 class="text-5xl font-bold text-center my-10">ecohcm</h1>
 
     <!-- 탭 -->
     <div class="flex justify-center border-b border-gray-700 mb-10">
